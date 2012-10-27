5942dddb169ee4dcda3db82ef75e097837d0382a
saga-games <saga-games@56274372-70c3-4bfc-bfc3-4c3a0b034d27>
2012-10-27 19:26:14 +0000

    Created tag for OpenMPT 1.20.04.00

---
+++ tags/1.20.04.00/common/misc_util.cpp
@@ -0,0 +1,85 @@
+/*
+ * misc_util.cpp
+ * -------------
+ * Purpose: Various useful utility functions.
+ * Notes  : (currently none)
+ * Authors: OpenMPT Devs
+ * The OpenMPT source code is released under the BSD license. Read LICENSE for more details.
+ */
+
+
+#include "stdafx.h"
+#include "misc_util.h"
+#include <ctime>
+
+/*
+ * Loads resource.
+ * [in] lpName and lpType: parameters passed to FindResource().
+ * [out] pData: Pointer to loaded resource data, nullptr if load not successful.
+ * [out] nSize: Size of the data in bytes, zero if load not succesfull.
+ * [out] hglob: HGLOBAL returned by LoadResource-function.
+ * Return: pData.
+ */
+LPCCH LoadResource(LPCTSTR lpName, LPCTSTR lpType, LPCCH& pData, size_t& nSize, HGLOBAL& hglob)
+//---------------------------------------------------------------------------------------------
+{
+	pData = nullptr;
+	nSize = 0;
+	hglob = nullptr;
+	HINSTANCE hInstance = AfxGetInstanceHandle();
+	HRSRC hrsrc = FindResource(hInstance, lpName, lpType); 
+	if (hrsrc != NULL)
+	{
+		hglob = LoadResource(hInstance, hrsrc);
+		if (hglob != NULL)
+		{
+			pData = reinterpret_cast<const char*>(LockResource(hglob));
+			nSize = SizeofResource(hInstance, hrsrc);
+		}
+	}
+	return pData;
+}
+
+
+// Returns WinAPI error message corresponding to error code returned by GetLastError().
+CString GetErrorMessage(DWORD nErrorCode)
+//---------------------------------------
+{
+	LPVOID lpMsgBuf;
+
+	FormatMessage(  FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+					NULL,
+					nErrorCode,
+					MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+					(LPTSTR)&lpMsgBuf,
+					0,
+					NULL );
+
+	CString msg = (LPTSTR)lpMsgBuf;
+	LocalFree(lpMsgBuf);
+
+	return msg;
+}
+
+
+std::basic_string<TCHAR> Util::sdTime::GetDateTimeStr()
+{
+	time_t t;
+	std::time(&t);
+	return _tctime(&t);
+}
+
+time_t Util::sdTime::MakeGmTime(tm& timeUtc)
+{
+#if (_MSC_VER < MSVC_VER_2005)
+	// VC++ 2003 doesn't have _mkgmtime
+	// This does not seem to work properly with DST time zones sometimes - if that's of any concern for you, please upgrade your compiler :)
+	TIME_ZONE_INFORMATION tzi;
+	GetTimeZoneInformation(&tzi);
+	const time_t timeUtcTimeT = mktime(&timeUtc) - tzi.Bias * 60;
+#else
+	const time_t timeUtcTimeT = _mkgmtime(&timeUtc);
+#endif
+	return timeUtcTimeT;
+}
+
---
+++ tags/1.20.04.00/soundlib/mod_specifications.cpp
@@ -0,0 +1,101 @@
+/*
+ * mod_specifications.cpp
+ * ----------------------
+ * Purpose: Mod specifications characterise the features of every editable module format in OpenMPT, such as the number of supported channels, samples, effects, etc...
+ * Notes  : (currently none)
+ * Authors: OpenMPT Devs
+ * The OpenMPT source code is released under the BSD license. Read LICENSE for more details.
+ */
+
+
+#include <stdafx.h>
+#include "mod_specifications.h"
+#include "../common/misc_util.h"
+
+
+MODTYPE CModSpecifications::ExtensionToType(LPCTSTR pszExt)
+//---------------------------------------------------------
+{
+	if (pszExt == nullptr)
+		return MOD_TYPE_NONE;
+	if (pszExt[0] == '.')
+		pszExt++;
+	char szExtA[CountOf(ModSpecs::mod.fileExtension)];
+	MemsetZero(szExtA);
+	const size_t nLength = _tcslen(pszExt);
+	if (nLength >= CountOf(szExtA))
+		return MOD_TYPE_NONE;
+	for(size_t i = 0; i < nLength; i++)
+		szExtA[i] = static_cast<char>(pszExt[i]);
+
+	for(size_t i = 0; i < CountOf(ModSpecs::Collection); i++)
+	{
+		if (!lstrcmpiA(szExtA, ModSpecs::Collection[i]->fileExtension))
+		{
+			return ModSpecs::Collection[i]->internalType;
+		}
+	}
+	// Special case: ITP files...
+	if(!lstrcmpi(szExtA, _T("itp")))
+	{
+		return MOD_TYPE_IT;
+	}
+
+	return MOD_TYPE_NONE;
+
+}
+
+
+bool CModSpecifications::HasNote(ModCommand::NOTE note) const
+//------------------------------------------------------------
+{
+	if(note >= noteMin && note <= noteMax)
+		return true;
+	else if(note >= NOTE_MIN_SPECIAL && note <= NOTE_MAX_SPECIAL)
+	{
+		if(note == NOTE_NOTECUT)
+			return hasNoteCut;
+		else if(note == NOTE_KEYOFF)
+			return hasNoteOff;
+		else if(note == NOTE_FADE)
+			return hasNoteFade;
+		else
+			return (internalType == MOD_TYPE_MPT);
+	} else if(note == NOTE_NONE)
+		return true;
+	return false;
+}
+
+
+bool CModSpecifications::HasVolCommand(ModCommand::VOLCMD volcmd) const
+//---------------------------------------------------------------------
+{
+	if(volcmd >= MAX_VOLCMDS) return false;
+	if(volcommands[volcmd] == '?') return false;
+	return true;
+}
+
+
+bool CModSpecifications::HasCommand(ModCommand::COMMAND cmd) const
+//----------------------------------------------------------------
+{
+	if(cmd >= MAX_EFFECTS) return false;
+	if(commands[cmd] == '?') return false;
+	return true;
+}
+
+
+char CModSpecifications::GetVolEffectLetter(ModCommand::VOLCMD volcmd) const
+//--------------------------------------------------------------------------
+{
+	if(volcmd >= MAX_VOLCMDS) return '?';
+	return volcommands[volcmd];
+}
+
+
+char CModSpecifications::GetEffectLetter(ModCommand::COMMAND cmd) const
+//---------------------------------------------------------------------
+{
+	if(cmd >= MAX_EFFECTS) return '?';
+	return commands[cmd];
+}
