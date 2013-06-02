bd616964290934f3a626bf0f15a27b67711223e4
manxorist <manxorist@56274372-70c3-4bfc-bfc3-4c3a0b034d27>
2013-06-02 12:27:48 +0000

    Merged revision(s) 2276-2287 from trunk/OpenMPT: [Fix] Fix broken endianness in C_Convert32To24. ........ [Ref] Add a simple int24 type to typedefs.h. Useful for reading or writing 24bit integer audio data. [Ref] Optionally support non-interleaved output in CSoundFile::Read. [Fix] Fix openmpt::module::read when rendering some GB of audio data at once on 64bit architectures. [Fix] Avoid allocating memory in render path in openmpt::module::read by using the new non-interleaved output of CSoundFile::Read. ........ [Ref] Rename mpt_strnicmp to mpt::strnicmp. ........ [Fix] GCC 4.4 does not want to instatiate template for anonymouse enums. Work-around it. ........ [Ref] Split tracker-only-related misc functions from common/misc_util into mptrack/MPTrackUtil. ........ [Fix] There were files missing in r2280. ........ [Ref] Silence 2 gcc 4.4 warnings in test.cpp. ........ [Ref] Fix -pedantic warnings in libopenmpt. ........ [Ref] Fix -pedantic warnings in libopenmpt. ........ [Ref] Remove unneeded reinterpret_cast. ........ [New] Try building a shared library via unix makefile. [Imp] Add dependency tracking to unix makefile. [Imp] Make make output more readable by default. ........ [Ref] Remove empty semicolons after closing namespace scope. [Ref] Remove comma at the end of enum members list in common/ (makes common/ gcc -pedantic clean). [Ref] sscanf %x expects pointer to unsigned int instead of int. ........

--- stdstring-names/soundlib/Load_mid.cpp
+++ stdstring-names/soundlib/Load_mid.cpp
@@ -739,7 +739,7 @@ bool CSoundFile::ReadMID(const BYTE *lpStream, DWORD dwMemLength, ModLoadingFlag
 							{
 								std::string s;
 								mpt::String::Read<mpt::String::maybeNullTerminated>(s, reinterpret_cast<const char*>(ptrk->ptracks), len);
-								if ((!mpt_strnicmp(s.c_str(), "Copyri", 6)) || s.empty()) break;
+								if ((!mpt::strnicmp(s.c_str(), "Copyri", 6)) || s.empty()) break;
 								if (i == 0x03)
 								{
 									if(songName.empty()) mpt::String::Copy(songName, s);
