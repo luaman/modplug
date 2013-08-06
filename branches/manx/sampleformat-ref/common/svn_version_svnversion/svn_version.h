d9bf7e32d3fed6be2fafde947b735db9a3175e55
manxorist <manxorist@56274372-70c3-4bfc-bfc3-4c3a0b034d27>
2013-08-06 11:53:18 +0000

    delete branch sampleformat-ref

--- branches/manx/sampleformat-ref/common/svn_version_svnversion/svn_version.h
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-
-#ifdef BUILD_SVNVERSION
-#define OPENMPT_VERSION_SVNVERSION BUILD_SVNVERSION
-#define OPENMPT_VERSION_DATE __DATE__ " " __TIME__
-#else
-#include "../svn_version_default/svn_version.h"
-#endif
