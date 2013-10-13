9351786ade473ac62e4eb4d16635336928659d92
manxorist <manxorist@56274372-70c3-4bfc-bfc3-4c3a0b034d27>
2013-10-13 10:28:05 +0000

    Merged revision(s) 2892 from trunk/OpenMPT: [Ref] sounddev: Explicitly pass SoundDeviceSettings to AudioRead and AudioDone.. [Ref] sounddev: Be more clear about samples vs frames in class ISoundDevice. ........

--- settings/mptrack/Mainfrm.h
+++ settings/mptrack/Mainfrm.h
@@ -333,8 +333,8 @@ public:
 
 	// from ISoundSource
 	void FillAudioBufferLocked(IFillAudioBuffer &callback);
-	void AudioRead(const SoundDeviceSettings &settings, PVOID pData, ULONG NumSamples);
-	void AudioDone(const SoundDeviceSettings &settings, ULONG NumSamples, int64 streamPosition);
+	void AudioRead(const SoundDeviceSettings &settings, std::size_t numFrames, void *buffer);
+	void AudioDone(const SoundDeviceSettings &settings, std::size_t numFrames, int64 streamPosition);
 	
 	bool audioTryOpeningDevice();
 	bool audioOpenDevice();
