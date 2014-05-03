aca81810f7044ed5f88a4357f23e6edccf5e06b2
saga-games <saga-games@56274372-70c3-4bfc-bfc3-4c3a0b034d27>
2014-05-03 21:33:05 +0000

    Created tag for OpenMPT 1.23.02.00

---
+++ tags/1.23.02.00/test/test.h
@@ -0,0 +1,18 @@
+/*
+ * test.h
+ * ------
+ * Purpose: Unit tests for OpenMPT.
+ * Notes  : We need FAAAAAAAR more unit tests!
+ * Authors: Olivier Lapicque
+ *          OpenMPT Devs
+ * The OpenMPT source code is released under the BSD license. Read LICENSE for more details.
+ */
+
+
+#pragma once
+
+namespace mpt { namespace Test {
+
+void DoTests();
+
+} } // namespace mpt::Test
---
+++ tags/1.23.02.00/unarchiver/unrar.h
@@ -0,0 +1,29 @@
+/*
+ * unrar.h
+ * -------
+ * Purpose: Header file for .rar loader
+ * Notes  : (currently none)
+ * Authors: OpenMPT Devs
+ * The OpenMPT source code is released under the BSD license. Read LICENSE for more details.
+ */
+
+#pragma once
+
+#include "archive.h"
+
+struct RARData;
+
+//====================================
+class CRarArchive : public ArchiveBase
+//====================================
+{
+protected:
+	friend struct RARData;
+	RARData *rarData;
+
+public:
+	CRarArchive(FileReader &file);
+	virtual ~CRarArchive();
+	
+	virtual bool ExtractFile(std::size_t index);
+};
