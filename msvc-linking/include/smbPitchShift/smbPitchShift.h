2a22a90b29bb59e439cdaf19b49d244e4b65f02d
manxorist <manxorist@56274372-70c3-4bfc-bfc3-4c3a0b034d27>
2014-09-07 09:05:13 +0000

    delete branch msvc-linking

--- msvc-linking/include/smbPitchShift/smbPitchShift.h
+++
@@ -1,7 +0,0 @@
-#ifndef SMBPITCHSHIFT_H
-#define SMBPITCHSHIFT_H
-
-extern void smbPitchShift(float pitchShift, long numSampsToProcess, long fftFrameSize, long osamp, float sampleRate, float *indata, float *outdata);
-#define MAX_FRAME_LENGTH 8192
-
-#endif
